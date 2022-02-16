6d183a94421a1d34d1cc9a6d50053b179fe5cfad
Evans998 <Karandeev-02@mail.ru>
2022-02-16 13:28:00 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import modal from './modules/modal'
+import modalService from './modules/modalService'
+import timer from './modules/timer'
+import scroll from './modules/scroll'
+import slider from './modules/slider'
+import calc from './modules/calc'
+
+modal()
+modalService()
+timer('20 february 2022')
+scroll()
+slider()
+calc()
